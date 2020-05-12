a9fb1f5fadcdc62cdb7d8de5966f59b8d672bc33
maryanaantonyuk <amaryana@gmail.com>
2020-05-12 16:31:29 -0700

    Initial commit

---
+++ src/1_TwoSum.kt
@@ -0,0 +1,22 @@
+fun main(nums: IntArray, target: Int): IntArray {
+
+
+        for (ai in nums.indices){
+
+            for ( bi in nums.indices){
+                var a= nums[ai]
+                var b = nums[bi]
+
+                if(ai != bi && a+b==target){
+
+
+
+                    return intArrayOf(ai,bi)
+
+                }
+            }
+        }
+        return intArrayOf(0)
+    }
+
+
